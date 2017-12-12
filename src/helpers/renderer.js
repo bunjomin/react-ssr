118b00789e9bf4c20a19fd297fbd397a3e2ab406
bunjomin <contact@ben-harrington.com>
2017-12-12 09:12:57 -0500

    Added G-Tag

--- src/helpers/renderer.js
+++ src/helpers/renderer.js
@@ -23,6 +23,15 @@ export default (req, store, context) => {
             <head>
                 ${helmet.title.toString()}
                 ${helmet.meta.toString()}
+                <!-- Global site tag (gtag.js) - Google Analytics -->
+                <script async src="https://www.googletagmanager.com/gtag/js?id=UA-106585018-1"></script>
+                <script>
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                
+                  gtag('config', 'UA-106585018-1');
+                </script>                
                 <link href="https://fonts.googleapis.com/css?family=Lato:100,300,300i,400,700,900" rel="stylesheet">
                 <link href="https://fonts.googleapis.com/css?family=Oswald:200" rel="stylesheet">
                 <link rel="stylesheet" href="styles/css/styles.css">
